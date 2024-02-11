4dd30011653d699fa157a9d52f390fa5687637ef
Allan Flores <itsallanflores06@gmail.com>
2024-02-10 22:48:45 -0500

    finished data responses and fixed test errors

--- src/dataResponses.js
+++ src/dataResponses.js
@@ -1,56 +1,119 @@
-const respond = (request, response, content, type) => {
-    response.writeHead(200, { 'Content-Type': type });
-    response.write(content);
-    response.end();
+const respond = (request, response, statusCode, type, content) => {
+  response.writeHead(statusCode, { 'Content-Type': type });
+  response.write(content);
+  response.end();
 };
 
-const makeJSONString = (data) => {
-    return JSON.stringify(data);
+const validateDataType = (type) => {
+  if (type === 'application/json' || type === 'text/xml') { return type; }
+
+  return 'application/json';
 };
+
 const makeXMLString = (data) => {
-    let xmlString = '<response>';
+  let xmlString = '<response>';
 
-    for (let key in data) {
-        xmlString += `<${key}>${data[key]}</${key}>`;
-    } 
+  Object.keys(data).forEach((key) => {
+    xmlString += `<${key}>${data[key]}</${key}>`;
+  });
+  //   for (const key of keys) {
+  //     xmlString += `<${key}>${data[key]}</${key}>`;
+  //   }
 
-    xmlString += `</response>`;
-    return xmlString;
+  // for (const key in data) {
+  //     xmlString += `<${key}>${data[key]}</${key}>`;
+  // }
+
+  xmlString += '</response>';
+  return xmlString;
 };
 const stringifyData = (type, data) => {
-    if (type == 'application/json')
-        return makeJSONString(data);
-    if (type == 'text/xml')
-        return makeXMLString(data);
+  if (type === 'text/xml') { return makeXMLString(data); }
+  return JSON.stringify(data);
 };
 
 const getSuccess = (request, response, type) => {
-    let data = {};
-    data.message = 'This is a successful response';
+  const data = {};
 
-    let content = stringifyData(type, data);
+  const dataType = validateDataType(type);
+  data.message = 'This is a successful response';
+  const content = stringifyData(dataType, data);
 
-    respond(request, response, content, type);
+  respond(request, response, 200, dataType, content);
 };
 const getBadRequest = (request, response, type, query) => {
+  const statusCode = query.valid === 'true' ? 200 : 400;
+  const data = {};
 
+  const dataType = validateDataType(type);
+  if (query.valid === 'true') { data.message = 'The request has the required parameters'; } else {
+    data.message = 'Missing valid query parameter set to true';
+    data.id = 'badRequest';
+  }
+  const content = stringifyData(dataType, data);
+
+  respond(request, response, statusCode, dataType, content);
 };
 const getUnauthorized = (request, response, type, query) => {
+  const statusCode = query.loggedIn === 'true' ? 200 : 401;
+  const data = {};
 
+  const dataType = validateDataType(type);
+  if (query.loggedIn === 'true') { data.message = 'You have successfully viewed the content'; } else {
+    data.message = 'Missing loggedIn query parameter set to yes';
+    data.id = 'unauthorized';
+  }
+  const content = stringifyData(dataType, data);
+
+  respond(request, response, statusCode, dataType, content);
 };
-const getForbidden = (request, response, type, query) => {
+const getForbidden = (request, response, type) => {
+  const data = {};
 
+  const dataType = validateDataType(type);
+  data.message = 'Missing valid query parameter set to true';
+  data.id = 'forbidden';
+  const content = stringifyData(dataType, data);
+
+  respond(request, response, 403, dataType, content);
 };
-const getInternal = (request, response, type, query) => {
+const getInternalServerError = (request, response, type) => {
+  const data = {};
 
+  const dataType = validateDataType(type);
+  data.message = 'Internal Servor Error. Something went wrong.';
+  data.id = 'internalError';
+  const content = stringifyData(dataType, data);
+
+  respond(request, response, 500, dataType, content);
 };
-const getNotImplemented = (request, response, type, query) => {
+const getNotImplemented = (request, response, type) => {
+  const data = {};
 
+  const dataType = validateDataType(type);
+  data.message = 'A get request for this page has not been implemented yet. Check again later for updated content.';
+  data.id = 'notImplemented';
+  const content = stringifyData(dataType, data);
+
+  respond(request, response, 501, dataType, content);
 };
-const getNotFound = (request, response, type, query) => {
+const getNotFound = (request, response, type) => {
+  const data = {};
 
+  const dataType = validateDataType(type);
+  data.message = 'The page you are looking for was not found.';
+  data.id = 'notFound';
+  const content = stringifyData(dataType, data);
+
+  respond(request, response, 404, dataType, content);
 };
 
 module.exports = {
-    getSuccess,
+  getSuccess,
+  getBadRequest,
+  getUnauthorized,
+  getForbidden,
+  getInternalServerError,
+  getNotImplemented,
+  getNotFound,
 };
--- src/server.js
+++ src/server.js
@@ -1,41 +1,34 @@
 const http = require('http');
 const url = require('url');
+const query = require('querystring');
 const htmlHandler = require('./htmlResponses.js');
 const dataHandler = require('./dataResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-    '/': htmlHandler.getIndex,
-    '/style.css': htmlHandler.getCSS,
-    '/success': dataHandler.getSuccess,
-    // '/badRequest': ,
-    // '/unauthorized': ,
-    // '/forbidden': ,
-    // '/internal': ,
-    // '/notImplemented': ,
-    // '/notFound': ,
-    notFound: htmlHandler.getIndex,
-
+  '/': htmlHandler.getIndex,
+  '/style.css': htmlHandler.getCSS,
+  '/success': dataHandler.getSuccess,
+  '/badRequest': dataHandler.getBadRequest,
+  '/unauthorized': dataHandler.getUnauthorized,
+  '/forbidden': dataHandler.getForbidden,
+  '/internal': dataHandler.getInternalServerError,
+  '/notImplemented': dataHandler.getNotImplemented,
+  '/notFound': dataHandler.getNotFound,
 };
 
 const onRequest = (request, response) => {
-    const parsedUrl = url.parse(request.url);
-    // console.log(parsedUrl);
-    // console.log(parsedUrl.pathname);
+  const parsedUrl = url.parse(request.url);
+  const handler = urlStruct[parsedUrl.pathname];
+  const queryParams = query.parse(parsedUrl.query);
+  let acceptedTypes;
 
-    const acceptedTypes = request.headers.accept.split(',');
+  if (!request.headers.accept) { acceptedTypes = ['application/json']; } else { acceptedTypes = request.headers.accept.split(','); }
 
-    const handler = urlStruct[parsedUrl.pathname];
-
-    if (handler) {
-        handler(request, response, acceptedTypes[0]);
-    }
-    else {
-        urlStruct['notFound'](request, response);
-    }
+  if (handler) { handler(request, response, acceptedTypes[0], queryParams); } else { urlStruct['/notFound'](request, response, acceptedTypes[0], queryParams); }
 };
 
 http.createServer(onRequest).listen(port, () => {
-    console.log(`Listening on 127.0.0.1: ${port}`);
+  // console.log(`Listening on 127.0.0.1: ${port}`);
 });
